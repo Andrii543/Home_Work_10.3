09b5fe281c1e535911a26317a3d8cfacd2030632
Andrii543 <andrii.rusyn15@gmail.com>
2024-12-06 18:44:46 +0100

    Remove spaces

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Home_Work_10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+
+            string inputUser = Console.ReadLine();
+            StringBuilder builderInpuUser = new StringBuilder();
+
+            foreach(char item in inputUser)
+            {
+                if(item == ' ')
+                {
+                    builderInpuUser.Append(item);
+                }
+            }
+
+
+            Console.WriteLine($"Результат : {builderInpuUser}");
+
+
+        }
+    }
+}
